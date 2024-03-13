5961696dcf70ab6475223826333c0be065c7ad54
Osaretin Igbinobaro <mail@osaretin.dev>
2024-03-13 12:06:09 +0000

    feat: add react refresh

--- webpack/webpack.dev.js
+++ webpack/webpack.dev.js
@@ -1,4 +1,10 @@
+const webpack = require("webpack");
+const ReactRefreshWebpackPlugin = require("@pmmmwh/react-refresh-webpack-plugin");
 module.exports = {
 	mode: "development",
+	devServer: {
+		hot: true,
+	},
 	devtool: "cheap-module-source-map",
+	plugins: [new ReactRefreshWebpackPlugin()],
 };
